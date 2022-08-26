654334efbd4457705225a9f2407fa989628bdda9
Roger Killer <roger.killer@advery.ch>
2022-08-26 20:59:33 +0200

    test commit

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,27 +4,14 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="lg:text-center">
         <h2
-          class="
-            text-base text-indigo-600
-            font-semibold
-            tracking-wide
-            uppercase
-          "
+          class="text-base text-indigo-600 font-semibold tracking-wide uppercase"
         >
           base-frontend
         </h2>
         <p
-          class="
-            mt-2
-            text-3xl
-            leading-8
-            font-extrabold
-            tracking-tight
-            text-gray-900
-            sm:text-4xl
-          "
+          class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl"
         >
-          A Nuxt Frontend for quicker starts
+          A Nuxt Frontend for quicker starts for dummys
         </p>
         <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
           To list a bit, the green ones are already installed, the reds might
@@ -35,25 +22,16 @@
 
       <div class="mt-10">
         <dl
-          class="
-            space-y-10
-            md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10
-          "
+          class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10"
         >
           <div v-for="feature in features" :key="feature.name" class="relative">
             <dt>
               <div
-                class="
-                  absolute
-                  flex
-                  items-center
-                  justify-center
-                  h-12
-                  w-12
-                  rounded-md
-                  text-white
-                "
-                :class="{ 'bg-green-500': feature.installed, 'bg-orange-500': !feature.installed }"
+                class="absolute flex items-center justify-center h-12 w-12 rounded-md text-white"
+                :class="{
+                  'bg-green-500': feature.installed,
+                  'bg-orange-500': !feature.installed,
+                }"
               >
                 <component
                   :is="feature.icon"
