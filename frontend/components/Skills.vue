a6f059a868b051df3356b3eace5c64dd600d714e
Roger Killer <roger.killer@advery.ch>
2022-09-20 23:01:46 +0200

    add url to skills

--- frontend/components/Skills.vue
+++ frontend/components/Skills.vue
@@ -7,15 +7,21 @@ const { data: skills } = await useFetch(
 </script>
 
 <template>
-  <div v-if="skills" class="mt-8">
+  <div v-if="skills" class="mt-8 mx-auto max-w-sm">
     <h2 class="text-3xl text-center tracking-tight font-bold mb-8">Skills</h2>
     <ul class="flex flex-wrap justify-center">
       <li
         v-for="skill in skills.data"
         :key="skill.id"
-        class="bg-white text-secondary w-max p-2 mr-4 mt-4"
+        class="bg-white text-xs text-secondary w-max p-2 mr-4 mt-4"
       >
-        {{ skill.attributes.skill }}
+        <a
+          v-if="skill.attributes.url !== null"
+          :href="skill.attributes.url"
+          target="_blank"
+          >{{ skill.attributes.title }}</a
+        >
+        <span v-else>{{ skill.attributes.title }}</span>
       </li>
     </ul>
   </div>
