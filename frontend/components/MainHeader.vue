02a36c809cc56050036b49a40a8cd31ebf215b25
Roger Killer <roger.killer@advery.ch>
2022-09-23 18:07:12 +0200

    Set link Advery

--- frontend/components/MainHeader.vue
+++ frontend/components/MainHeader.vue
@@ -8,7 +8,14 @@
           Hello I'm Roger.
         </h1>
         <p class="max-w-xl mt-5 mx-auto text-xl mb-8">
-          Welcome to my site, I am a Frontend Developer
+          Welcome to my site, I am a Frontend Developer at
+          <a
+            class="underline decoration-2"
+            href="https://www.advery.ch/"
+            target="_blank"
+            >Advery</a
+          >
+          and I like to building things.
         </p>
         <MainSocials />
         <Skills />
