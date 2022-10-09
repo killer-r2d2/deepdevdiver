bc17e42f10f0aa582298dfe022c80a6f98b2c499
Roger Killer <roger.killer@advery.ch>
2022-10-09 19:16:18 +0200

    implement load more. Remove pinia

--- frontend/components/Skills.vue
+++ frontend/components/Skills.vue
@@ -1,26 +1,93 @@
-<script setup>
-import { useSkillStore } from "@/stores/SkillStore";
-const skillStore = useSkillStore();
-skillStore.fetchSkills();
+<script>
+import qs from "qs";
+
+export default {
+  data() {
+    return {
+      skills: [],
+      page: 1,
+      pageSize: 3,
+      hasMoreSkills: true,
+      showSkills: false,
+      show: false,
+    };
+  },
+  mounted() {
+    this.loadMoreSkills();
+  },
+
+  methods: {
+    loadMoreSkills() {
+      const query = qs.stringify(
+        {
+          pagination: {
+            page: this.page,
+            pageSize: this.pageSize,
+          },
+        },
+        { encodeVluesOnly: true }
+      );
+
+      const url = `http://localhost:1337/api/skills?${query}`;
+
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          this.skills = this.skills.concat(data.data);
+          this.hasMoreSkills =
+            data.meta.pagination.page < data.meta.pagination.pageCount;
+          this.page++;
+        });
+    },
+  },
+};
 </script>
 
 <template>
-  <div v-if="skillStore.skills.data" class="mt-8 mx-auto max-w-sm">
-    <h2 class="text-3xl text-center tracking-tight font-bold mb-8">Skills</h2>
-    <ul class="flex flex-wrap justify-center">
-      <li
-        v-for="skill in skillStore.skills.data"
-        :key="skill.id"
-        class="bg-white text-xs text-secondary w-max p-2 mr-4 mt-4"
-      >
-        <a
-          v-if="skill.attributes.url !== null"
-          :href="skill.attributes.url"
-          target="_blank"
-          >{{ skill.attributes.title }}</a
+  <div>
+    <div v-if="skills" class="mt-8 mx-auto max-w-sm">
+      <h2 class="text-3xl text-center tracking-tight font-bold mb-8">Skills</h2>
+      <div v-if="skills">
+        <ul class="flex flex-wrap justify-center">
+          <TransitionGroup name="fade">
+            <li
+              v-for="skill in skills"
+              :key="skill.id"
+              class="bg-white text-xs text-secondary w-max p-2 mr-4 mt-4"
+            >
+              <a
+                v-if="skill.attributes.url !== null"
+                :href="skill.attributes.url"
+                target="_blank"
+                >{{ skill.attributes.title }}</a
+              >
+              <p v-else>{{ skill.attributes.title }}</p>
+            </li>
+          </TransitionGroup>
+        </ul>
+
+        <button
+          v-if="hasMoreSkills"
+          @click="loadMoreSkills"
+          type="button"
+          aria-label="Load more skills"
+          class="text-secondary mt-8 inline-flex items-center rounded-full border border-transparent bg-white p-3 text-slate shadow-sm hover:bg-slate-50 focus:ring-2 focus:ring-slate-300"
         >
-        <span v-else>{{ skill.attributes.title }}</span>
-      </li>
-    </ul>
+          <PlusIcon class="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- frontend/stores/SkillStore.js
+++ frontend/stores/SkillStore.js
@@ -1,20 +1,20 @@
-import { defineStore } from "pinia";
+// import { defineStore } from "pinia";
 
-export const useSkillStore = defineStore("SkillStore", {
-  state: () => ({
-    skills: [],
-  }),
-  actions: {
-    async fetchSkills() {
-      try {
-        const url = "http://localhost:1337/api/skills";
-        const response = await fetch(url);
-        const skills = await response.json();
-        this.skills = skills;
-      } catch (error) {
-        console.log(error);
-        return error;
-      }
-    },
-  },
-});
+// export const useSkillStore = defineStore("SkillStore", {
+//   state: () => ({
+//     skills: [],
+//   }),
+//   actions: {
+//     async fetchSkills() {
+//       try {
+//         const url = "http://localhost:1337/api/skills";
+//         const response = await fetch(url);
+//         const skills = await response.json();
+//         this.skills = skills;
+//       } catch (error) {
+//         console.log(error);
+//         return error;
+//       }
+//     },
+//   },
+// });
